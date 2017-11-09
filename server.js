375531b6aca07f5c0ee47709f4a3ae6820f9aff2
Brett Fuller <bfuller123@gmail.com>
2017-11-08 22:36:52 -0600

    added in twilio to server.js

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 var express = require('express');
+var twilio = require('twilio');
+
+var accountSSID = null;
+var authToken = null;
+
+var client = twilio(accountSSID, authToken);
+
+client.messages.create({
+  body: 'This is a test from eatUp!',
+  to: '8177068356',
+  from: '+18722013712'
+})
+.then((message) => console.log(message.sid))
 
 var app = express();
 const PORT = process.env.PORT || 3000;
