3e120f73dd776ef90e6a10425301752272619cbb
Brett Fuller <bfuller123@gmail.com>
2017-11-15 00:50:36 -0600

    random restaurant always returns results

--- config/database.js
+++ config/database.js
@@ -1,21 +1,21 @@
 // config/database.js
 
-// module.exports = {
-//     'connection': {
-//         'host': 'mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//         'user': 'mzojztfzqeb1t44h',
-//         'password': 'woxc0lt5dsuttc1j'
-//     },
-// 	'database': 'e0a737o4nssgt75u',
-//     'users_table': 'users'
-// };
-
 module.exports = {
     'connection': {
-        'host': '127.0.0.1',
-        'user': 'prodUser',
-        'password': 'password'
+        'host': 'mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'user': 'mzojztfzqeb1t44h',
+        'password': 'woxc0lt5dsuttc1j'
     },
-	'database': 'eatUp',
+	'database': 'e0a737o4nssgt75u',
     'users_table': 'users'
 };
+
+// module.exports = {
+//     'connection': {
+//         'host': '127.0.0.1',
+//         'user': 'prodUser',
+//         'password': 'password'
+//     },
+// 	'database': 'eatUp',
+//     'users_table': 'users'
+// };
