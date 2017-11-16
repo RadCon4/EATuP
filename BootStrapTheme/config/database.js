15246fe5b471dc17a5bd201e2ac1637c29e1295b
Brett Fuller <bfuller123@gmail.com>
2017-11-16 11:13:26 -0600

    add page theme works

--- BootStrapTheme/config/database.js
+++ BootStrapTheme/config/database.js
@@ -2,57 +2,57 @@
 module.exports = {
     'connection': {
         'host': '127.0.0.1',
-        'user': 'root',
-        'password': 'Leahkr_12'
+        'user': 'prodUser',
+        'password': 'password'
     },
-	'database': 'db_users_password_test',
+	'database': 'eatUp',
     'users_table': 'users'
 };
 // sequelize code below
 /*
 {
- 
+
     "development": {
- 
+
         "username": "root",
- 
+
         "password": null,
- 
+
         "database": "sequelize_passport",
- 
+
         "host": "127.0.0.1",
- 
+
         "dialect": "mysql"
- 
+
     },
- 
+
     "test": {
- 
+
         "username": "",
- 
+
         "password": null,
- 
+
         "database": "",
- 
+
         "host": "",
- 
+
         "dialect": "mysql"
- 
+
     },
- 
+
     "production": {
- 
+
         "username": "",
- 
+
         "password": null,
- 
+
         "database": "",
- 
+
         "host": "127.0.0.1",
- 
+
         "dialect": "mysql"
- 
+
     }
- 
+
 }
 */
